4e802351dcb767a36e5e99b24859a774a9a29b9d
gdev <Gautam.Dev@siriusxm.com>
2017-04-12 14:32:01 -0500

    Added click on open options.

--- popup.js
+++ popup.js
@@ -5,66 +5,124 @@ var urls = {
 
 function clickHandler(e) {
 
-    var urlsToOpen = document.getElementsByName("link");
-    urlsToOpen.forEach(function (toOpen){
-        if(toOpen.checked == true) {
-            chrome.tabs.create({url: toOpen.value, active : false });
+    var allUrls = document.getElementsByName("link");
+    var urlsToOpen = [];
+    allUrls.forEach (function (url) {
+        console.log(url);
+        if (url.checked) {
+            urlsToOpen.push(url.value);
         }
     });
+    // alert("kkk" + urlsToOpen);
+    var firstUrl = urlsToOpen.shift();
+
+    urlsToOpen.forEach(function (url) {
+        chrome.tabs.create({url: url, active : false});
+    });
+
+        // chrome.tabs.create({url: firstUrl, active : false}, function (tab) {
+        // alert(firstUrl);
+
+            // urlsToOpen.forEach(function (url) {
+            //     chrome.tabs.create({url: url, active : false});
+            // });
+
+        // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+            // alert("in update");
+            // alert(tab.url + "kkk" + firstUrl);
+
+            /*if (tab.url === firstUrl && changeInfo.status == 'complete') {*/
+             // alert('done');
+             // urlsToOpen.forEach(function (toOpen){
+             // chrome.tabs.create({url: toOpen, active : false});
+             // });
+             //return;
+             //}
+        // });
+    // });
+    // alert(" first" + firstUrl);
+
     window.close();
+}
+function openTab(urlToOpen) {
+    chrome.tabs.create({url: urlToOpen, active : false});
+    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+        // alert("in update");
+        // alert(tab.url + "kkk" + urlToOpen);
+
+        if (tab.url === urlToOpen && changeInfo.status == 'complete') {
+            alert('done');
+            return;
+        }
+    });
+
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('openbtn').addEventListener('click', clickHandler);
     var content = document.getElementById('content');
+    chrome.tabs.getCurrent(function (callback) {
+        console.log(callback);
+    });
+
+    chrome.tabs.getSelected(null, function(tab){
+        console.log(tab);
+    });
 
     getCurrentTabUrl(function(currentUrl) {
         var domain = getDomain(currentUrl);
         var name = domain.split('.')[0];
-        var allurls = Object.keys(urls);
+        //console.log("fff" + JSON.stringify());
+        //var allurls = Object.keys(urls);
+        //console.log(allurls);
+        //allurls.forEach(function(url, index) {
+        //if (url === name) {
+        var allurls = urls[name];
+        //console.log(index);
+        //var pages = Object.values(urls)[index];
+        var list = document.createElement("UL");
+        var i=1;
+        var value="";
+        allurls.forEach(function(page) {
+            console.log(Object.keys(page)[0]);
+            console.log(Object.values(page)[0]);
+            var input = document.createElement("INPUT");
+            var linkObj = Object.values(page)[0];
 
-        allurls.forEach(function(url, index) {
-            if (url === name) {
-                console.log(index);
-                var pages = Object.values(urls)[index];
-                var list = document.createElement("UL");
-                var i=1;
-                var value="";
-                pages.forEach(function(page) {
-                    console.log(Object.keys(page)[0]);
-                    console.log(Object.values(page)[0]);
-                    var input = document.createElement("INPUT");
-                    input.setAttribute("type", "checkbox");
-                    var linkObj = Object.values(page)[0];
-                    input.setAttribute("name", "link");
+            input.setAttribute("type", "checkbox");
+            input.setAttribute("value", Object.values(page)[0]);
+            input.setAttribute("name", "link");
+            var id = Object.keys(page)[0];
+            input.setAttribute("id", id);
 
-                    if (typeof linkObj === 'object') {
-                        input.setAttribute("value", linkObj.url);
-                        input.setAttribute("Alt", linkObj.alt);
-                        if (currentUrl !== linkObj && linkObj.selected !== false) {
-                            input.setAttribute("checked", true);
-                        }
-                    } else {
-                        input.setAttribute("value", linkObj);
-                        input.setAttribute("Alt", linkObj);
-                        if (currentUrl !== linkObj) {
-                            input.setAttribute("checked", true);
-                        }
-                    }
-                    var id = Object.keys(page)[0];
-                    input.setAttribute("id", id);
-
-                    var label = document.createElement('label');
-                    label.htmlFor = id;
-                    label.appendChild(document.createTextNode(id));
-                    list.appendChild(input);
-                    list.appendChild(label);
-                    var line = document.createElement('br');
-                    list.appendChild(line);
-                });
-                content.appendChild(list);
+            if (typeof linkObj === 'object') {
+                input.setAttribute("value", linkObj.url);
+                input.setAttribute("Alt", linkObj.alt);
+                if (currentUrl !== linkObj && linkObj.selected !== false) {
+                    input.setAttribute("checked", true);
+                }
+            } else {
+                input.setAttribute("value", linkObj);
+                input.setAttribute("Alt", linkObj);
+                if (currentUrl !== linkObj[0]) {
+                    input.setAttribute("checked", true);
+                }
             }
+
+
+            var label = document.createElement('label');
+            label.htmlFor = id;
+            label.appendChild(document.createTextNode(id));
+            list.appendChild(input);
+            list.appendChild(label);
+            var line = document.createElement('br');
+            list.appendChild(line);
+
+
         });
+        content.appendChild(list);
+        //}
+        //});
     });
 
 });
@@ -99,5 +157,4 @@ function getCurrentTabUrl(callback) {
         var url = tab.url;
         callback(url);
     });
-
 }
