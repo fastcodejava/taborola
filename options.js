0526dd63be690b79f6f7ae6486bbd1ef30c0bc25
gdev <Gautam.Dev@siriusxm.com>
2017-04-18 16:32:30 -0500

    Added options.js for options page.

--- options.js
+++ options.js
@@ -2,32 +2,190 @@
  * Created by gdev on 3/18/2017.
  */
 function save_options() {
-    var tabsBackground = document.getElementById('tabsBackground').checked;
-    var highlightTabs = document.getElementById('highlightTabs').checked;
+    var prefDomain = document.getElementById('prefDomain').value;
+    var prefUrl = document.getElementById('prefUrl').value;
+    //var highlightTabs = document.getElementById('highlightTabs').checked;
+    alert(prefDomain);
+    var urls={};
 
-    chrome.storage.sync.set({
-        tabsBackground: tabsBackground,
-        highlightTabs: highlightTabs
-    }, function() {
-        // Update status to let user know options were saved.
-        var status = document.getElementById('status');
-        status.textContent = 'Options saved.';
-        setTimeout(function() {
-            status.textContent = '';
-        }, 750);
+    urls[prefDomain] = [prefUrl];
+
+    chrome.storage.sync.get( "preferedPages", function(items) {
+        alert("items" + JSON.stringify(items));
+        var origObj = items.preferedPages;
+        alert("before" + JSON.stringify(origObj));
+
+        if (origObj[prefDomain]) {
+            origObj[prefDomain].push(prefUrl);
+        } else {
+            origObj[prefDomain] = [prefUrl];
+        }
+
+        urls = origObj;
+        alert("final--" + JSON.stringify(urls));
+        chrome.storage.sync.set({preferedPages: origObj}, function() {
+            // Update status to let user know options were saved.
+            var status = document.getElementById('status');
+            status.textContent = 'Options saved.';
+            setTimeout(function() {
+                status.textContent = '';
+            }, 750);
+        });
+        //items.preferedPages.prefDomain.push(prefUrl);
+        //alert("after changing" + items.preferedPages.prefDomain.push(prefUrl));
     });
+
+    /*	var urls;
+     chrome.storage.sync.get( "preferedPages", function(items) {
+     alert("items" + JSON.stringify(items));
+     if (items) {
+     var domain = items.preferedPages.prefDomain;
+     alert(domain + "--" + prefDomain)
+     if (domain) {
+     //urls = domain.push(prefUrl);
+     urls = items.preferedPages.prefDomain.push(prefUrl);
+     alert("see" + urls);
+     } else {
+     alert ("me here");
+     urls[prefDomain] = [prefUrl];
+     }
+
+
+     }
+     });*/
+
+
+    //alert( "URLS" + JSON.stringify(urls));
+    /*chrome.storage.sync.set({
+     preferedPages: urls
+     }, function() {
+     // Update status to let user know options were saved.
+     var status = document.getElementById('status');
+     status.textContent = 'Options saved.';
+     setTimeout(function() {
+     status.textContent = '';
+     }, 750);
+     });*/
 }
 
 // Restores tabsBackground state using the preferences
 // stored in chrome.storage.
 function restore_options() {
-    chrome.storage.sync.get({
-        tabsBackground: true,
-        highlightTabs: true
-    }, function(items) {
-        document.getElementById('tabsBackground').checked = items.tabsBackground;
-        document.getElementById('highlightTabs').checked = items.highlightTabs;
+    chrome.storage.sync.get( "preferedPages", function(items) {
+        var content = document.getElementById('content');
+        var text = document.createTextNode(JSON.stringify(items));
+        var data = items.preferedPages;
+        var table = document.createElement("TABLE");
+        table.setAttribute("id", "table");
+        content.appendChild(table);
+        var tr = document.createElement("TR");
+        tr.setAttribute("id", "heading");
+        table.appendChild(tr);
+
+        var col1 = document.createElement("TD");
+        var cell1 = document.createTextNode("Domain");
+        col1.appendChild(cell1);
+        tr.appendChild(col1);
+
+        var col2 = document.createElement("TD");
+        var cell2 = document.createTextNode("Preferred URL");
+        col2.appendChild(cell2);
+        tr.appendChild(col2);
+
+        var col3 = document.createElement("TD");
+        var cell3 = document.createTextNode("Delete");
+        col3.appendChild(cell3);
+        tr.appendChild(col3);
+
+        table.style.border =  "thin solid #000000";
+        //tr.style.border =  "thin solid #000000";
+        col1.style.border =  "thin solid #000000";
+        col2.style.border =  "thin solid #000000";
+        col3.style.border =  "thin solid #000000";
+        var rowIndex = 0;
+        Object.keys(data).forEach(function(domain, index){
+            var rowSpan = (data[domain]).length;
+            data[domain].forEach(function(url, url_index) {
+                var dataRow = document.createElement("TR");
+                dataRow.id = rowIndex;
+                table.appendChild(dataRow);
+                if (url_index === 0) {
+                    var col1 = document.createElement("TD");
+                    col1.rowSpan = rowSpan;
+                    var cell1 = document.createTextNode(domain);
+                    col1.appendChild(cell1);
+                    col1.style.border =  "thin solid #000000";
+                    dataRow.appendChild(col1);
+                }
+                var col2 = document.createElement("TD");
+                var cell2 = document.createTextNode(url);
+                col2.appendChild(cell2);
+                col2.style.border =  "thin solid #000000";
+                dataRow.appendChild(col2);
+                var col3 = document.createElement("TD");
+                var cell3 = document.createElement("INPUT");
+                cell3.type = "checkbox";
+                cell3.id = domain;
+                cell3.name = "delete_chkbx";
+                col3.appendChild(cell3);
+                col3.align = "center";
+                col3.style.border =  "thin solid #000000";
+                dataRow.appendChild(col3);
+                rowIndex++;
+            });
+
+        });
+        //content.appendChild(text);
+
     });
+}
+
+function delete_selected_options() {
+    alert("in delete");
+    var allChBx = document.getElementsByName("delete_chkbx");
+
+    chrome.storage.sync.get( "preferedPages", function(items) {
+        alert("items" + JSON.stringify(items));
+        var origObj = items.preferedPages;
+        allChBx.forEach (function (chkbx) {
+            //alert(chkbx);
+            if (chkbx.checked) {
+                //alert("chked");
+                var rowObj = chkbx.parentElement.parentElement;
+                var rowId = chkbx.parentElement.parentElement.id;
+                var domain;
+                var url;
+                //alert(rowObj.childNodes.length);
+                if (rowObj.childNodes.length === 2) {
+                    domain = chkbx.id;
+                    url = rowObj.childNodes[0].innerHTML;
+                } else {
+                    domain = rowObj.childNodes[0].innerHTML;
+                    url = rowObj.childNodes[1].innerHTML;
+                }
+                alert(rowId + "--" + domain + "--" + url);
+                alert("orig obj" + JSON.stringify(origObj));
+                var urlIndex = origObj[domain].indexOf(url);
+                alert("index in obj-" + urlIndex);
+                origObj[domain].splice(urlIndex, 1);
+                alert("obj after delete" + JSON.stringify(origObj));
+            }
+        });
+        alert("obj after delete" + JSON.stringify(origObj));
+        chrome.storage.sync.set({preferedPages: origObj}, function() {
+            // Update status to let user know options were saved.
+            var status = document.getElementById('status');
+            status.textContent = 'Options saved.';
+            setTimeout(function() {
+                status.textContent = '';
+            }, 750);
+        });
+        //items.preferedPages.prefDomain.push(prefUrl);
+        //alert("after changing" + items.preferedPages.prefDomain.push(prefUrl));
+    });
+
+
 }
 document.addEventListener('DOMContentLoaded', restore_options);
 document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('delete').addEventListener('click', delete_selected_options);
--- popup.js
+++ popup.js
@@ -1,143 +1,86 @@
-var jsonData;
-
-var xhr = new XMLHttpRequest();
-xhr.open('GET', chrome.extension.getURL('pages.json'), true);
-xhr.onreadystatechange = function() {
-    console.log("here");
-    if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
-        console.log(xhr.responseText);
-        jsonData = JSON.parse(xhr.responseText);
-    }
+var urls = {
+    "amazon": [
+        {"Books": "http://www.amazon.in/books"},
+        {"Kitchen": "http://www.amazon.in/kitchen"},
+        {"Grocery": "http://www.amazon.in/Gourmet-Specialty-Foods"},
+        {"Mobile-Phones": "http://www.amazon.in/mobile-phones"}
+    ],
+    "cnn" : [
+        {"Regions": "http://edition.cnn.com/regions"},
+        {"Politics": "http://edition.cnn.com/politics"},
+        {"International": "http://money.cnn.com/INTERNATIONAL/"},
+        {"Entertainment": "http://edition.cnn.com/entertainment"},
+        {"Technology": "http://money.cnn.com/technology/"}
+    ]
 };
-xhr.send();
 
-function closeWindow (e) {
-    window.close();
-}
 
 function clickHandler(e) {
-
 
     var allUrls = document.getElementsByName("link");
     var urlsToOpen = [];
     allUrls.forEach (function (url) {
         console.log(url);
         if (url.checked) {
-            urlsToOpen.push(url.value);
+            chrome.tabs.create({url: url.value, active : false});
         }
     });
-    // alert("kkk" + urlsToOpen);
-    // var firstUrl = urlsToOpen.shift();
 
-    urlsToOpen.forEach(function (url) {
-        chrome.tabs.create({url: url, active : false});
-    });
 
-        // chrome.tabs.create({url: firstUrl, active : false}, function (tab) {
-        // alert(firstUrl);
 
-            // urlsToOpen.forEach(function (url) {
-            //     chrome.tabs.create({url: url, active : false});
-            // });
-
-        // chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-            // alert("in update");
-            // alert(tab.url + "kkk" + firstUrl);
-
-            /*if (tab.url === firstUrl && changeInfo.status == 'complete') {*/
-             // alert('done');
-             // urlsToOpen.forEach(function (toOpen){
-             // chrome.tabs.create({url: toOpen, active : false});
-             // });
-             //return;
-             //}
-        // });
-    // });
-    // alert(" first" + firstUrl);
 
     window.close();
 }
-function openTab(urlToOpen) {
-    chrome.tabs.create({url: urlToOpen, active : false});
-    chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
-        // alert("in update");
-        // alert(tab.url + "kkk" + urlToOpen);
-
-        if (tab.url === urlToOpen && changeInfo.status == 'complete') {
-            alert('done');
-            return;
-        }
-    });
-
+function closeWindow (e) {
+    window.close();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
     document.getElementById('cancelbtn').addEventListener('click', closeWindow);
     document.getElementById('openbtn').addEventListener('click', clickHandler);
-
     var content = document.getElementById('content');
-    chrome.tabs.getCurrent(function (callback) {
-        console.log(callback);
-    });
 
-    chrome.tabs.getSelected(null, function(tab){
-        console.log(tab);
-    });
+    var jsonData;
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', chrome.extension.getURL('mypages.json'), true);
+    xhr.onreadystatechange = function() {
+        console.log("here");
+        if(xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            console.log(xhr.responseText);
+            jsonData = JSON.parse(xhr.responseText);
+        }
+    };
+    xhr.send();
 
     getCurrentTabUrl(function(currentUrl) {
         var domain = getDomain(currentUrl);
         var name = domain.split('.')[0];
-        //console.log("fff" + JSON.stringify());
-        //var allurls = Object.keys(urls);
-        //console.log(allurls);
-        //allurls.forEach(function(url, index) {
-        //if (url === name) {
         var allurls = jsonData[name];
-        //console.log(index);
-        //var pages = Object.values(urls)[index];
-        var list = document.createElement("UL");
-        var i=1;
-        var value="";
-        allurls.forEach(function(page) {
-            console.log(Object.keys(page)[0]);
-            console.log(Object.values(page)[0]);
-            var input = document.createElement("INPUT");
-            var linkObj = Object.values(page)[0];
+        console.log(jsonData[name]);
+        if(jsonData[name]){
+            var list = document.createElement("UL");
+            var i=1;
+            var value="";
+            allurls.forEach(function(page) {
+                console.log(Object.keys(page)[0]);
+                console.log(Object.values(page)[0]);
+                var input = document.createElement("INPUT");
+                input.setAttribute("type", "checkbox");
+                input.setAttribute("value", Object.values(page)[0]);
+                input.setAttribute("name", "link");
+                var id = Object.keys(page)[0];
+                input.setAttribute("id", id);
 
-            input.setAttribute("type", "checkbox");
-            input.setAttribute("value", Object.values(page)[0]);
-            input.setAttribute("name", "link");
-            var id = Object.keys(page)[0];
-            input.setAttribute("id", id);
-
-            if (typeof linkObj === 'object') {
-                input.setAttribute("value", linkObj.url);
-                input.setAttribute("Alt", linkObj.alt);
-                if (currentUrl !== linkObj && linkObj.selected !== false) {
-                    input.setAttribute("checked", true);
-                }
-            } else {
-                input.setAttribute("value", linkObj);
-                input.setAttribute("Alt", linkObj);
-                if (currentUrl !== linkObj[0]) {
-                    input.setAttribute("checked", true);
-                }
-            }
-
-
-            var label = document.createElement('label');
-            label.htmlFor = id;
-            label.appendChild(document.createTextNode(id));
-            list.appendChild(input);
-            list.appendChild(label);
-            var line = document.createElement('br');
-            list.appendChild(line);
-
-
-        });
-        content.appendChild(list);
-        //}
-        //});
+                var label = document.createElement('label');
+                label.htmlFor = id;
+                label.appendChild(document.createTextNode(id));
+                list.appendChild(input);
+                list.appendChild(label);
+                var line = document.createElement('br');
+                list.appendChild(line);
+            });
+            content.appendChild(list);
+        }
     });
 
 });
@@ -166,10 +109,11 @@ function getCurrentTabUrl(callback) {
         currentWindow: true
     };
 
-    chrome.tabs.query(queryInfo, function (tabs) {
+    chrome.tabs.query(queryInfo, function(tabs) {
 
         var tab = tabs[0];
         var url = tab.url;
         callback(url);
     });
+
 }
